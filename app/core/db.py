deb69355fa2cd801f9730200b0000641fa4d7f8e
Nutelliks <naudangaliev@gmail.com>
2026-08-13 15:49:32 +0500

    refactor: improve app structure

--- app/core/db.py
+++ app/core/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-from ..config import settings
+from .config import settings
 
 async_engine = create_async_engine(
     url=settings.DATABASE_URL,
